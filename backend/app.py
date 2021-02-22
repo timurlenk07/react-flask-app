cff545b342cf1cef5ba1f343c9e5117387e92990
Timurlenk07 <tmarcikka@gmail.com>
2021-02-22 19:27:40 +0100

    Register form now talks with backend

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,15 @@
 import time
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    print(f"Got values: {request.json}")
+    return {'success': False, "msg": "Keep up the good work :)"}
--- src/userHandling/Register.js
+++ src/userHandling/Register.js
@@ -47,6 +47,22 @@ function RegisterForm(props) {
     validationSchema: schema,
     onSubmit: (values) => {
       console.log(values)
+      fetch('#', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(values)//JSON.stringify({ title: 'React Hooks POST Request Example' })
+      }).then(response => response.json())
+        .then(response => {
+          console.log('Registration success response was: ' + response.success)
+          if (response.success) {
+            window.location.href = '/register'
+          } else {
+            console.log('Maybe next time...' + response.msg)
+          }
+        })
+        .catch(error => {
+          console.error('There was an error!', error)
+        })
     }
   })
 
