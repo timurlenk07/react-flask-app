6b39204b40b7eb975e6ef1fdae5d6844e2e1f362
Timurlenk07 <tmarcikka@gmail.com>
2021-02-23 20:22:33 +0100

    Registration is now checked on backend

--- backend/flasKingBackend/register.py
+++ backend/flasKingBackend/register.py
@@ -1,17 +1,46 @@
-from flask import Blueprint, request, current_app
+from flask import Blueprint, request
+from marshmallow import Schema, fields, validate
+from pymongo.errors import WriteError
+from webargs.flaskparser import use_kwargs
 
 from flasKingBackend.db import get_db_client
 
 bp = Blueprint('register', __name__)
 
 
+class CreateRegistrationSchema(Schema):
+    registrationType = fields.String(required=True, validate=validate.OneOf(['magánszemély', 'cég']))
+    fullName = fields.String(required=True)
+    email = fields.String(required=True, validate=validate.Email())
+    address = fields.String(required=True)
+    phone = fields.Number(required=True)
+    password = fields.String(required=True, validate=validate.Length(min=8))
+    birthDate = fields.DateTime(required=True)
+    terms = fields.Boolean(required=True, validate=validate.Equal(True))
+
+
+# TODO: DEBUG only
+@bp.before_request
+def log_post_body():
+    if request.method == 'POST':
+        print(f"Got POST body: {request.json}")
+
+
 @bp.route('/register', methods=['POST'])
-def register_user():
-    print(f"Got values: {request.json}")
+@use_kwargs(CreateRegistrationSchema())
+def register_user(**kwargs):
+    try:
+        client = get_db_client()
+        registrations = client.users.registrations
+        reg_id = registrations.insert_one(kwargs).inserted_id
+        print(f"ID of inserted object: {reg_id}")
+        return {'success': True}
+    except WriteError as err:
+        return {'success': False, 'msg': err}
 
-    client = get_db_client()
-    registrations = client.users.registrations
-    reg_id = registrations.insert_one(request.json).inserted_id
-    print(f"ID of inserted object: {reg_id}")
 
-    return {'success': False, "msg": "Keep up the good work :)"}
+@bp.errorhandler(422)
+def validation_error(err):
+    """Handles 422 errors"""
+    print(err.data)
+    return err.data.get('messages').get('json')
