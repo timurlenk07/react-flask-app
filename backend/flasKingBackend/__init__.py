0a422853e869e94152f23a15627b97435a3ca5bc
Timurlenk07 <tmarcikka@gmail.com>
2021-02-23 17:18:04 +0100

    Added environment config loading

--- backend/flasKingBackend/__init__.py
+++ backend/flasKingBackend/__init__.py
@@ -1,14 +1,11 @@
-import os
-
 from flask import Flask
 
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
---
+++ backend/flasKingBackend/config.py
@@ -0,0 +1,4 @@
+from os import environ
+
+DB_USER = environ.get('DB_USER')
+DB_PASSWORD = environ.get('DB_PASSWORD')
